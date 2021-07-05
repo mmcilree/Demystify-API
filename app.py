247d77effc00ec8b360e983b41c79f614a34dd9b
mmcilree <mjm42@st-andrews.ac.uk>
2021-07-05 10:32:57 +0100

    Added URL prefix.

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ from rq import Queue
 from worker import conn
 from routes import bp as routes
 
+BASE_URL = "/demystify/api"
 app = Flask(__name__)
 CORS(app)
 
 q = Queue(connection=conn)
 
 logging.basicConfig(level=logging.DEBUG)
-app.register_blueprint(routes)
+app.register_blueprint(routes, url_prefix=BASE_URL)
 
 if __name__ == "__main__":      
     app.run()                     
